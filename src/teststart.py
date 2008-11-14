2e206202881caef16ab8981a9b691072c4ab3bf9
k_mashiko <k_mashiko@k_mashiko.mshome.net>
2008-11-14 21:45:28 +0900

    11/14

--- src/teststart.py
+++ src/teststart.py
@@ -5,21 +5,17 @@
 #
 from testtool import TestTool
 
-def test():
-    xmldir = "../xml"
-    testfile = "%s/test.xml" % xmldir
-    reportfile = "%s/report.xml" % xmldir
+xmldir = "../xml"
+testfile = "%s/test.xml" % xmldir
+reportfile = "%s/report.xml" % xmldir
 
-    tool = TestTool()
-    tool.loadXml(testfile)
-    try:
-        tool.run()
-    except:
-        tool.saveXml(reportfile)
-        raise
-
-if __name__ == "__main__":
-    test()
+tool = TestTool()
+tool.loadXml(testfile)
+try:
+    tool.run()
+except:
+    tool.saveXml(reportfile)
+    raise
 
 #
 # EOF
